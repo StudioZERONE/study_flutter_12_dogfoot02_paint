4b26a51c53225b2923d2fa97d75b1286e63bf8c9
ZERONE <zerone012@gmail.com>
2023-07-21 10:23:33 +0900

    UI는 잡았는데 그림이 안 그려짐

--- lib/screen/drawing_page/drawing_page.dart
+++ lib/screen/drawing_page/drawing_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:study_flutter_12_dogfoot02_paint/model/dot_info.dart';
 import 'package:study_flutter_12_dogfoot02_paint/screen/drawing_page/local_utils/drawing_provider.dart';
 
 class DrawingPage extends StatefulWidget {
@@ -14,84 +15,108 @@ class _DrawingPageState extends State<DrawingPage> {
   Widget build(BuildContext context) {
     var p = Provider.of<DrawingProvider>(context);
     return Scaffold(
-      appBar: AppBar(title: const Text('')),
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(child: Container()),
-            Divider(
-              height: 1,
-              thickness: 1,
-              color: Colors.grey[400],
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned.fill(
+                    child: CustomPaint(
+                  painter: DrawingPainter(p.lines),
+                )),
+                GestureDetector(
+                  behavior: HitTestBehavior.translucent,
+                  onPanStart: (s) {
+                    if (p.eraseMode) {
+                      p.erase(s.localPosition);
+                    } else {
+                      p.drawStart(s.localPosition);
+                    }
+                  },
+                  onPanUpdate: (s) {
+                    if (p.eraseMode) {
+                      p.erase(s.localPosition);
+                    } else {
+                      p.drawing(s.localPosition);
+                    }
+                  },
+                  child: Container(),
+                ),
+              ],
             ),
-            Container(
-              color: Colors.grey[700],
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).padding.bottom),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(
-                      top: 20,
-                      bottom: 20,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        _colorWidget(Colors.black),
-                        _colorWidget(Colors.red),
-                        _colorWidget(Colors.yellow),
-                        _colorWidget(Colors.green),
-                        _colorWidget(Colors.blue),
-                      ],
-                    ),
+          ),
+          Divider(
+            height: 1,
+            thickness: 1,
+            color: Colors.grey[400],
+          ),
+          Container(
+            color: Colors.grey[700],
+            padding:
+                EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                    top: 20,
+                    bottom: 20,
                   ),
-                  Row(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            left: 20,
-                            right: 5,
-                          ),
-                          child: Slider(
-                            activeColor: Colors.white,
-                            inactiveColor: Colors.white,
-                            value: p.size,
-                            onChanged: (size) {
-                              p.changeSize = size;
-                            },
-                            min: 3,
-                            max: 15,
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        behavior: HitTestBehavior.translucent,
-                        onTap: () {
-                          p.changeEraseMode();
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 25),
-                          child: Text(
-                            '지우개',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                                fontWeight: p.eraseMode
-                                    ? FontWeight.w900
-                                    : FontWeight.w300),
-                          ),
-                        ),
-                      ),
+                      _colorWidget(Colors.black),
+                      _colorWidget(Colors.red),
+                      _colorWidget(Colors.yellow),
+                      _colorWidget(Colors.green),
+                      _colorWidget(Colors.blue),
                     ],
                   ),
-                ],
-              ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20,
+                          right: 5,
+                        ),
+                        child: Slider(
+                          activeColor: Colors.white,
+                          inactiveColor: Colors.white,
+                          value: p.size,
+                          onChanged: (size) {
+                            p.changeSize = size;
+                          },
+                          min: 3,
+                          max: 15,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      behavior: HitTestBehavior.translucent,
+                      onTap: () {
+                        p.changeEraseMode();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 25),
+                        child: Text(
+                          '지우개',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: p.eraseMode
+                                  ? FontWeight.w900
+                                  : FontWeight.w300),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -111,12 +136,48 @@ class _DrawingPageState extends State<DrawingPage> {
           border: p.color == color
               ? Border.all(
                   color: Colors.white,
-                  width: 2,
+                  width: 4,
                 )
               : null,
           color: color,
         ),
       ),
     );
+  }
+}
+
+class DrawingPainter extends CustomPainter {
+  DrawingPainter(this.lines);
+
+  final List<List<DotInfo>> lines;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (var oneLine in lines) {
+      Color color = Colors.white;
+      double size = 1.0;
+      var l = <Offset>[];
+      var p = Path();
+
+      for (var oneDot in oneLine) {
+        color ??= oneDot.color;
+        size ??= oneDot.size;
+        l.add(oneDot.offset);
+      }
+
+      p.addPolygon(l, false);
+      canvas.drawPath(
+          p,
+          Paint()
+            ..color = color
+            ..strokeWidth = size
+            ..strokeCap = StrokeCap.round
+            ..style = PaintingStyle.stroke);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
